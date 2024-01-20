06a56ddb32971141c07c1285db410f885728a558
Duc <duc4nh.nguyen@gmail.com>
2024-01-20 12:05:40 +0700

    New template from Duc Anh Nguyen for INS2065

---
+++ docker/command.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+rm -f tmp/pids/server.pid
+
+bundle check || bundle install --binstubs=$BUNDLE_BIN
+
+bundle exec yarn install --check-files
+
+#bundle exec yarn build
+
+rails db:prepare
+
+#rails db:migrate
+
+#rails assets:precompile
+
+#bundle exec rails s -p 3000 -b 0.0.0.0
